6dccf80fa55a543eecf640a41a9f7c72404e194f
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-06-29 08:13:16 +0300

    Add Flask API with SQLAlchemy serialization and association proxy

--- server/app.py
+++ server/app.py
@@ -1,7 +1,32 @@
-# server/app.py
+# # server/app.py
 
-from flask import Flask, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
+# from flask import Flask, jsonify, make_response
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+# from models import db, User, Review, Game
+
+# app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.json.compact = False
+
+# migrate = Migrate(app, db)
+
+# db.init_app(app)
+
+# @app.route('/')
+# def index():
+#     return "Index for Game/Review/User API"
+
+# # start building your API here
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+
+from flask import Flask, make_response
 from flask_migrate import Migrate
 
 from models import db, User, Review, Game
@@ -9,19 +34,45 @@ from models import db, User, Review, Game
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
+app.json.compact = False  # nicer JSON formatting
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Index for Game/Review/User API"
 
-# start building your API here
+
+@app.route("/games")
+def games():
+    games = [game.to_dict() for game in Game.query.all()]
+    response = make_response(games, 200)
+    return response
 
 
-if __name__ == '__main__':
+@app.route("/games/<int:id>")
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    if not game:
+        return make_response({"error": "Game not found"}, 404)
+    game_dict = game.to_dict()
+    response = make_response(game_dict, 200)
+    return response
+
+
+@app.route("/games/users/<int:id>")
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    if not game:
+        return make_response({"error": "Game not found"}, 404)
+
+    users = [user.to_dict(rules=("-reviews",)) for user in game.users]
+    response = make_response(users, 200)
+    return response
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
-
